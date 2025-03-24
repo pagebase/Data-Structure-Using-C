d2c34cd88af4f89387bac7df4db161a5e35964cf
originalop <rswami5579@gmail.com>
2025-03-24 14:23:18 +0530

    Added Delete functions

--- Practice_Codes/Fifth_Q.cpp
+++ Practice_Codes/Fifth_Q.cpp
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-void ins();
-void inorder(node *);
 
 struct node
 {
@@ -10,14 +8,24 @@ struct node
     node *right;
 } *root;
 
+void ins();
+void inorder(node *);
+void del(int);
+void delOne(node *, node *);
+void delTwo(node *);
+void delLeaf(node *);
+
 int main()
 {
+    int choice;
+    int choice1;
+    int delValue;
     while (true)
     {
-        print("1.Insert\n");
-        print("2. Inorder\n");
-        print("3. Delete\n    1. Delete leaf node\n");
-        print("4. Exit\n");
+        printf("1. Insert\n");
+        printf("2. Inorder\n");
+        printf("3. Delete\n");
+        printf("4. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
         switch (choice)
@@ -29,20 +37,12 @@ int main()
             inorder(root);
             break;
         case 3:
-            switch (3)
-            {
-            case 1:
-                delLeaf();
-                break;
-            case 2:
-                del_one();
-                break;
-            case 3:
-                delTwo();
-                break;
-            }
+            printf("Enter delete value: ");
+            scanf("%d", &delValue);
+            del(delValue);
+            break;
         case 4:
-            exit(5);
+            exit(0);
         }
     }
     return 0;
@@ -57,11 +57,12 @@ void ins()
     int insVal;
     node *newNode;
     node *temp;
+    newNode = create();
     printf("Enter insert value: ");
     scanf("%d", &insVal);
     newNode->left = NULL;
     newNode->info = insVal;
-    neNode->right = NULL;
+    newNode->right = NULL;
     if (root == NULL)
     {
         root = newNode;
@@ -108,5 +109,120 @@ void inorder(node *p)
         inorder(p->left);
         printf("%d\t", p->info);
         inorder(p->right);
+    }
+}
+
+// Delete functions
+void delLeaf(node *p, node *c)
+{
+    if (p->left == c)
+    {
+        p->left = NULL;
+    }
+    else if (p->right == c)
+    {
+        p->right = NULL;
+    }
+    free(c);
+    printf("Node successfully deleted\n");
+}
+
+void delOne(node *p, node *c)
+{
+    if (c == p->left)
+    {
+        if (c->left != NULL)
+        {
+            p->left = c->left;
+        }
+        else
+        {
+            p->left = c->right;
+        }
+    }
+    else if (c == p->right)
+    {
+        if (c->right != NULL)
+        {
+            p->right = c->right;
+        }
+        else
+        {
+            p->right = c->left;
+        }
+    }
+    free(c);
+    printf("Node successfully deleted\n");
+}
+
+void delTwo(node *c)
+{
+    node *lft, *p = NULL;
+    lft = c->left;
+    while (lft->right != NULL)
+    {
+        p = lft;
+        lft = lft->right;
+    }
+
+    c->info = lft->info;
+    if (p == NULL)
+    {
+        p = c;
+    }
+
+    if (lft->left == NULL && lft->right == NULL)
+    {
+        delLeaf(p, lft);
+    }
+    else
+    {
+        delOne(p, lft);
+    }
+}
+
+void del(int delValue)
+{
+    int found = 0;
+    node *p;
+    node *c;
+    p = root;
+    c = root;
+    while (c != NULL)
+    {
+        if (c->info == delValue)
+        {
+            found = 1;
+            break;
+        }
+        else if (delValue < c->info)
+        {
+            p = c;
+            c = c->left;
+        }
+        else if (delValue > c->info)
+        {
+            p = c;
+            c = c->right;
+        }
+    }
+    if (found)
+    {
+        if (c->left == NULL && c->right == NULL)
+        {
+            delLeaf(p, c);
+        }
+        else if (c->left != NULL && c->right != NULL)
+        {
+            delTwo(c);
+        }
+        else
+        {
+            delOne(p, c);
+        }
+    }
+    else
+    {
+        printf("Not Not Found!\n");
     }
 }
--- Practice_Codes/temp.cpp
+++ Practice_Codes/temp.cpp
@@ -1,63 +1,226 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-void moveValueToRightSide(int arr[], int *arraySize, int firstIndex, int lastIndex);
-void getInputByUser(int arr[], int *arraySize, int firstIndex, int lastIndex);
-
-int main()
+#include <alloc.h>
+#include <conio.h>
+#include <process.h>
+struct node
 {
-    int num;
-    printf("Enter array size: ");
-    scanf("%d", &num);
+    node *left;
+    int info;
+    node *right;
+} *root;
+node *create();
+void insert(int);
+void inorder(node *);
+void del_leaf(node *, node *);
+void del_one(node *, node *);
+void del_two(node *);
+void del(int);
 
-    int firstIndex = 0;
-    int lastIndex = num - 1;  // Index should be num-1 to fit the last element
-    int arraySize = num;
-
-    int *arr = (int *)malloc((num * 2) * sizeof(int)); // Allocate double the memory initially
-    if (!arr) {  // Check if malloc was successful
-        printf("Memory allocation failed.\n");
-        return -1;
-    }
-
-    printf("firstIndex: %d\n", firstIndex);
-    printf("lastIndex: %d\n", lastIndex);
-    printf("Array size: %d\n", arraySize);
-
-    getInputByUser(arr, &arraySize, firstIndex, lastIndex);
-
-    // Free the allocated memory
-    free(arr);
-
-    return 0;
+void main()
+{
+    int ch, n, a;
+    clrscr();
+    do
+    {
+        printf("\nEnter your choice ");
+        printf("\n1:Insert\n2:Inorder\n3:Delete\n4:Exit ");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            printf("\nEnter val to insert:  ");
+            scanf("%d", &n);
+            insert(n);
+            break;
+        case 2:
+            inorder(root);
+            break;
+        case 3:
+            printf("Enter val to delete=");
+            scanf("%d", &n);
+            del(n);
+            break;
+        case 4:
+            exit(1);
+        }
+    } while (ch != 4);
+    getch();
 }
 
-void getInputByUser(int arr[], int *arraySize, int firstIndex, int lastIndex)
+node *create()
 {
-    for (int i = 0; i < *arraySize; i++)
+    node *z;
+    z = (node *)malloc(sizeof(node));
+    return (z);
+}
+
+void insert(int x)
+{
+    node *p, *temp;
+    p = create();
+    p->left = NULL;
+    p->info = x;
+    p->right = NULL;
+    if (root == NULL)
     {
-        printf("Enter [%d]th value: ", i);
-        scanf("%d", &arr[i]);
-        moveValueToRightSide(arr, arraySize, firstIndex, lastIndex);
-        
-        // Update array size if you are shifting elements
-        arr[*arraySize] = arr[i] * arr[i];  // Store the square of the number at the next position
-        (*arraySize)++;  // Increase array size
+        root = p;
+        printf("\nNode is inserted..");
+    }
+    else
+    {
+        temp = root;
+
+        while (temp != NULL)
+        {
+            if (p->info < temp->info)
+            {
+                if (temp->left == NULL)
+                {
+                    temp->left = p;
+                    printf("\nNode is inserted...");
+                    break;
+                }
+                else
+                {
+                    temp = temp->left;
+                }
+            }
+            else if (p->info > temp->info)
+            {
+                if (temp->right == NULL)
+                {
+                    temp->right = p;
+                    printf("\nNode is inserted...");
+                    break;
+                }
+                else
+                {
+                    temp = temp->right;
+                }
+            }
+        }
     }
 }
 
-void moveValueToRightSide(int arr[], int *arraySize, int firstIndex, int lastIndex)
+void inorder(node *p)
 {
-    // Shift values right from the end
-    for (int i = *arraySize - 1; i > lastIndex; i--)
+    if (p != NULL)
     {
-        arr[i] = arr[i - 1];
+        inorder(p->left);
+        printf("\t%d", p->info);
+        inorder(p->right);
+    }
+}
+
+void del_leaf(node *p, node *c)
+{
+    if (c == p->left)
+    {
+        p->left = NULL;
+    }
+    else if (c == p->right)
+    {
+        p->right = NULL;
+    }
+    free(c);
+    printf("\n Node is deleted...");
+}
+void del_one(node *p, node *c)
+{
+    if (c == p->left)
+    {
+        if (c->left != NULL)
+        {
+            p->left = c->left;
+        }
+        else
+        {
+            p->left = c->right;
+        }
+    }
+    else if (c == p->right)
+    {
+        if (c->right != NULL)
+        {
+            p->right = c->right;
+        }
+        else
+        {
+            p->right = c->left;
+        }
+    }
+    free(c);
+    printf("\nNOde is deleted..");
+}
+
+void del_two(node *c)
+{
+    node *lft, *p = NULL;
+    lft = c->left;
+    while (lft->right != NULL)
+    {
+        p = lft;
+        lft = lft->right;
     }
 
-    // Print the array contents
-    for (int i = 0; i < *arraySize; i++)
+    c->info = lft->info;
+    if (p == NULL)
     {
-        printf("%d\t", arr[i]);
+        p = c;
     }
-    printf("\n");
+
+    if (lft->left == NULL && lft->right == NULL)
+    {
+        del_leaf(p, lft);
+    }
+    else
+    {
+        del_one(p, lft);
+    }
+}
+
+void del(int x)
+{
+    int f = 0;
+    node *c, *p;
+    p = c = root;
+
+    while (c != NULL)
+    {
+        if (x == c->info)
+        {
+            f = 1;
+            break;
+        }
+        else if (x < c->info)
+        {
+            p = c;
+            c = c->left;
+        }
+        else if (x > c->info)
+        {
+            p = c;
+            c = c->right;
+        }
+    }
+
+    if (f == 1)
+    {
+        if (c->left == NULL && c->right == NULL)
+        {
+            del_leaf(p, c);
+        }
+        else if (c->left != NULL && c->right != NULL)
+        {
+            del_two(c);
+        }
+        else
+        {
+            del_one(p, c);
+        }
+    }
+    else
+    {
+        printf("\nNode is NOT found ");
+    }
 }
